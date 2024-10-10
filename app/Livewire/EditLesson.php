0777f2d3fa96b94cd6a0effd2aa81bc9c1a1a53a
burrodecarga <edwinhenriquezh@hotmail.com>
2024-10-09 20:02:47 -0400

    pendientes

---
+++ app/Livewire/EditLesson.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class EditLesson extends Component
+{
+    public $openLesson=false;
+    public $lesson,$description;
+
+    public function mount(Lesson $lesson){
+        $this->lesson = $lesson;
+         }
+
+
+    public function render()
+    {
+        return view('livewire.edit-lesson');
+    }
+}
